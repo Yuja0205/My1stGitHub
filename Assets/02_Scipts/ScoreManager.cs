cb41c8158e5473f0a59f2c105a1c616439a8f1d5
Yuja0205 <youjaehyun2@gmail.com>
2025-07-25 16:49:52 +0900

    폰트
    다양한 폰트들 테스트중

--- Assets/02_Scipts/ScoreManager.cs
+++ Assets/02_Scipts/ScoreManager.cs
@@ -8,13 +8,15 @@ public class ScoreManager : MonoBehaviour
 {
     public GameManager gamemanager;
     public Text scoreText;
+    public Text bestscoreText;
     public Text endScore;
 
-    public int score;
-
+    public int score;//점수숫자표시
+    string key = "bestScore"; // 오타방지
     void Start()
     {
         UpdateScoreText();
+        //PlayerPrefs.DeleteAll();// 데이터 삭제 테스트
     }
 
     public void AddScore()
@@ -25,6 +27,36 @@ public class ScoreManager : MonoBehaviour
 
     void UpdateScoreText()
     {
+        if (gamemanager.currentStage == 5)
+        {
+            // 최고 점수가 있다면
+            if (PlayerPrefs.HasKey(key))
+            {
+                // 최고 점수 가져오기
+                int best = PlayerPrefs.GetInt(key);
+                // 최고 점수 < 현재 점수
+                if (best < score)
+                {
+                    // 현재 점수를 최고 점수에 저장한다.
+                    PlayerPrefs.SetInt(key, score);
+                    bestscoreText.text = score.ToString(); // 현재 점수가 최고 점수이니까 time 값을 넣어도 됨
+                }
+                else // 최고 점수 > 현재 점수
+                {
+                    bestscoreText.text = best.ToString(); // best가 여전히 최고 점수니까
+                }
+            }
+            else // 최고 점수가 없다면
+            {
+                // 현재 점수를 저장한다.
+                PlayerPrefs.SetInt(key, score);
+                bestscoreText.text = score.ToString();
+            }
+        }
+    
+        
+    
+
         //if (scoreText != null)
         //{
         //    scoreText.text = $"{score}";
@@ -45,6 +77,8 @@ public class ScoreManager : MonoBehaviour
             scoreText.text = $"{score} / {gamemanager.stageScore[gamemanager.currentStage - 1]}";
             endScore.text = $"{score} / {gamemanager.stageScore[gamemanager.currentStage - 1]}";
         }
+
+
     }
     
 }
